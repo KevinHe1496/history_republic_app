d1b96c93687c3e5bf1fe73a50c3c80f6f6a37743
Kevin Heredia <kevin@ravecodesolutions.com>
2025-03-31 17:32:35 -0500

    Initial Commit

---
+++ History Republic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  History Republic
+//
+//  Created by Kevin Heredia on 31/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ History Republic/History_RepublicApp.swift
@@ -0,0 +1,17 @@
+//
+//  History_RepublicApp.swift
+//  History Republic
+//
+//  Created by Kevin Heredia on 31/3/25.
+//
+
+import SwiftUI
+
+@main
+struct History_RepublicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
