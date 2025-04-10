1ad4ea261787c9f842d1b6e3ddcdbfc6bc211028
Kevin Heredia <kevin@ravecodesolutions.com>
2025-04-10 11:57:59 -0500

    Custom tabBar added

--- History Republic/Shared/Presentation/Views/TabBar/TabBarView.swift
+++ History Republic/Shared/Presentation/Views/TabBar/TabBarView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct TabBarView: View {
+    
+    init() {
+        // Create a custom appearance
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        
+        // Use your custom color as the background
+        appearance.backgroundColor = UIColor(Color.greenSecondaryColor)
+        
+        // Selected icon and text: mainBrownColor
+        appearance.stackedLayoutAppearance.selected.iconColor = UIColor(Color.mainBrownColor)
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [
+            .foregroundColor: UIColor(Color.mainBrownColor)
+        ]
+        
+        // Unselected icon and text: white
+        appearance.stackedLayoutAppearance.normal.iconColor = .white
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [
+            .foregroundColor: UIColor.white
+        ]
+        
+        // Apply appearance to the TabBar
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
+
+    
     var body: some View {
         
         TabView {
+            
             Tab("Home", systemImage: "house.fill") {
                 PodcastsView()
             }
@@ -21,6 +49,7 @@ struct TabBarView: View {
             
             Tab("Game", systemImage: "gamecontroller.fill") {
                 GameView()
+                    .padding()
             }
         }
     }
