0d797861cc73e7d8a339c00e2b626451db668ef9
Kevin Heredia <kevin@ravecodesolutions.com>
2025-04-11 19:13:44 -0500

    register view added

--- History Republic/Shared/Presentation/Views/Register/RegisterView.swift
+++ History Republic/Shared/Presentation/Views/Register/RegisterView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            // MARK: - Logo (Top)
+            VStack {
+                Image(.logoHR)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300)
+            }
+            Spacer()
+            
+            VStack(spacing: 10) {
+                CustomTextField(placeholder: "Email", text: $email, keyboardType: .emailAddress)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
+                CustomSecureField(placeholder: "Password", password: $password)
+                
+                CustomButton(title: "Sign up", color: .mainBrown) {
+                    print(email)
+                    print(password)
+                }
+            }
+            Spacer()
+            
+        }
+        .padding(16)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.greenSecondary)
     }
 }
 
