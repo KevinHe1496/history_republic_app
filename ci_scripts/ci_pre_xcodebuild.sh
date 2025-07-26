5b030f0bfd93035e6088709379c2daf7fa4932b8
Heandy27 <andy_heredia2008@hotmail.com>
2025-07-26 16:12:22 -0500

    added script

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -5,3 +5,14 @@
 #
 #  Created by Andy Heredia on 26/7/25.
 #  
+
+echo 'Change Beta App Icon'
+
+APP_ICON_PATH="$CI_PRIMARY_REPOSITORY_PATH/History Republic/Assets.xcassets/AppIcon.appiconset"
+
+echo $APP_ICON_PATH
+
+rm -rf APP_ICON_PATH
+
+mv "$CI_PRIMARY_REPOSITORY_PATH/ci_script/AppIcon-Beta.appiconset" $APP_ICON_PATH
+
